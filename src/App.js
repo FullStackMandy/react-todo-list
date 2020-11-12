479b0a1d4682bbc9bfa8daaf39c97e6a4de5ab33
Mandy <FullStackMandy@gmail.com>
2020-11-12 13:26:23 +0100

    calendar to do

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Calendar from 'react-calendar';
+//import 'react-calendar/dist/Calendar.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
